2b6b383171c7919db46a70e1d4ec889f21671e31
eugene-mindset <eugene.asare999@gmail.com>
2023-08-15 12:20:23 -0400

    Create initial structure of the project.

---
+++ backend/run_backend.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ./backend
+
+if [ -d "./.venv" ] 
+then
+    . ./.venv/bin/activate
+    pip install -r ./requirements.txt
+else
+    python -m venv ./.venv
+    . ./.venv/bin/activate
+    pip install -r ./requirements.txt
+fi
+
+python ./main.py
+
+cd ..
